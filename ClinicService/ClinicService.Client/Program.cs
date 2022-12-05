543e58954dcd37f37c09b2a2ab195f583788dc08
Danil Shabanov <shabanov1903@gmail.com>
2022-12-05 21:48:04 +0300

    Review of lesson materials

---
+++ ClinicService/ClinicService.Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using ClinicServiceNamespace;
+using Grpc.Net.Client;
+using static ClinicServiceNamespace.ClinicService;
+
+AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+ClinicServiceClient clinicServiceClient = new ClinicServiceClient(channel);
+
+var createClientResponse = clinicServiceClient.CreateClinet(new CreateClientRequest
+{
+    Document = "DOC34 445",
+    FirstName = "Данил",
+    Patronymic = "Валерьевич",
+    Surname = "Шабанов"
+});
+
+if (createClientResponse.ErrCode == 0)
+{
+    Console.WriteLine($"Client #{createClientResponse.ClientId} created successfully.");
+}
+else
+{
+    Console.WriteLine($"Create client error.\nErrorCode: {createClientResponse.ErrCode}\nErrorMessage: {createClientResponse.ErrMessage}");
+}
+
+var getClientResponse = clinicServiceClient.GetClients(new GetClientsRequest());
+
+if (createClientResponse.ErrCode == 0)
+{
+    Console.WriteLine("Clients");
+    Console.WriteLine("=======\n");
+
+    foreach (var client in getClientResponse.Clients)
+    {
+        Console.WriteLine($"#{client.ClientId} {client.Document} {client.Surname} {client.FirstName} {client.Patronymic}");
+    }
+}
+else
+{
+    Console.WriteLine($"Get clients error.\nErrorCode: {getClientResponse.ErrCode}\nErrorMessage: {getClientResponse.ErrMessage}");
+}
+
+Console.ReadKey();
